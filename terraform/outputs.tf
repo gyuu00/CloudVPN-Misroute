435196f5f2faa7c772c19474c906c38b86befb35
9yu <159871693+gyuu00@users.noreply.github.com>
2025-07-12 22:25:45 +0900

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,29 +1,19 @@
-output "public_s3_bucket_url" {
-  description = "URL to the public S3 bucket containing the leaked VPN config"
-  value       = "https://${aws_s3_bucket.public_vpn.bucket}.s3.amazonaws.com/"
+output "vpn_credentials_file_url" {
+  description = "URL to the leaked VPN credentials"
+  value       = "https://${aws_s3_bucket.leak_bucket.bucket}.s3.amazonaws.com/credentials.txt"
 }
 
-output "vpn_credentials_file" {
-  description = "Path to the credentials.txt file"
-  value       = "https://${aws_s3_bucket.public_vpn.bucket}.s3.amazonaws.com/credentials.txt"
+output "vpn_config_file_url" {
+  description = "URL to the leaked VPN configuration file"
+  value       = "https://${aws_s3_bucket.leak_bucket.bucket}.s3.amazonaws.com/client.ovpn"
 }
 
-output "vpn_config_file" {
-  description = "Path to the client.ovpn file"
-  value       = "https://${aws_s3_bucket.public_vpn.bucket}.s3.amazonaws.com/client.ovpn"
+output "ec2_b_private_ip" {
+  description = "Private IP of EC2 instance in VPC-B (target instance)"
+  value       = aws_instance.ec2_b.private_ip
 }
 
-output "admin_server_ip" {
-  description = "EC2 public IP (reachable only via VPN)"
-  value       = aws_instance.admin_server.public_ip
-}
-
-output "flag_bucket_name" {
-  description = "Private S3 bucket containing the flag"
-  value       = aws_s3_bucket.private_flag.bucket
-}
-
-output "vpc_endpoint_id" {
-  description = "ID of the VPC Endpoint (used in IAM and S3 policies)"
-  value       = aws_vpc_endpoint.s3.id
+output "s3_bucket_with_flag" {
+  description = "Name of the S3 bucket that contains the flag"
+  value       = aws_s3_bucket.leak_bucket.id
 }
